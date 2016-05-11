df434d03acb57d70c65d7ca082d3570988c3e3a3
Khen Price <khen@humtap.com>
2016-05-11 15:36:10 -0400

    setting up production port for heroku

--- server/server.js
+++ server/server.js
@@ -12,8 +12,9 @@ app.use(express.static(path.join(__dirname, "../app/dist")));
 app.use(bodyParser.json())
 app.use("/api", schoolController);
 
-app.listen(7777, function () {
-    console.log("Started listening on port", 7777);
+var port = process.env.PORT || 12810;
+app.listen(port, function () {
+    console.log("Started listening on port", port);
 });
 
 // Connect to mongodb database
