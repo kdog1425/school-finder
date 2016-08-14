924a656fb1693db9d742252114c819ddc5588cb1
Khen Price <khen@humtap.com>
2016-08-14 14:30:13 -0400

    changed listening port

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "../app/dist")));
 app.use(bodyParser.json());
 app.use("/", audioUploadController);
 
-DEBUG = true;
+DEBUG = false;
 
 // log to console
 app.use(morgan('dev'));
